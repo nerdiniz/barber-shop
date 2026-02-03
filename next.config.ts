2fc27b6cb682a42556f58ad34490128cc19e8de1
Marcelo Diniz dos Santos <nerdiniz@outlook.com>
2026-02-03 17:18:15 -0300

    Adicionando dir out.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // If your GitHub repository is NOT 'username.github.io', uncomment and update the line below:
+  basePath: '/barber-shop',
 };
 
 export default nextConfig;
